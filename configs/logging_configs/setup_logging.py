02ad7ed61e5a4931a0b4cab3b4efa73d4072ba96
lance hester <lhestergs4@gmail.com>
2025-05-19 17:02:02 -0400

    improve comment messages to follow convention space after # symbol and syntax fixes

--- configs/logging_configs/setup_logging.py
+++ configs/logging_configs/setup_logging.py
@@ -11,14 +11,14 @@ def configure_loggers() -> None:
     """
     Configure loggers for the Flask application
     """
-    #Get json logger configuration file
+    # Get JSON logger configuration file
     app_base_path = str(Path(__file__).resolve().parent.parent.parent)
     json_config_file = os.path.join(app_base_path, "configs/logging_configs/logger_configure.json")
 
     with open(json_config_file) as f:
         config = json.load(f)
 
-    #get absolute path for log directory and .jsonl file
+    # Get absolute path for log directory and .jsonl file
     config['handlers']['file_json']['filename'] = os.path.join(app_base_path, config['handlers']['file_json']['filename'])
 
     logging.config.dictConfig(config)
