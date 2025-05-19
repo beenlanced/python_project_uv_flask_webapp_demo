65efb064bbb43a5e40060e578ec7f7442db72d5c
lance hester <lhestergs4@gmail.com>
2025-05-19 16:58:40 -0400

    improve obtaining path to logs folder and underlying json configuration

--- configs/logging_configs/setup_logging.py
+++ configs/logging_configs/setup_logging.py
@@ -2,22 +2,25 @@ import atexit
 import json
 import logging
 import logging.config
+import os
 
 from pathlib import Path
-
 
 
 def configure_loggers() -> None:
     """
     Configure loggers for the Flask application
     """
-    json_config_path =  Path(__file__).resolve().parent.parent.parent /"configs/logging_configs"
-    json_config_path = json_config_path if json_config_path.is_absolute() else Path(json_config_path.resolve())
-    json_config_file = list(json_config_path.glob("*.json", case_sensitive=False))[0]
+    #Get json logger configuration file 
+    app_base_path = str(Path(__file__).resolve().parent.parent.parent)
+    json_config_file = os.path.join(app_base_path, "configs/logging_configs/logger_configure.json")
 
     with open(json_config_file) as f:
         config = json.load(f)
-    
+
+    #get absolute path for log directory and .jsonl file
+    config['handlers']['file_json']['filename'] = os.path.join(app_base_path, config['handlers']['file_json']['filename'])
+
     logging.config.dictConfig(config)
     queue_handler = logging.getHandlerByName("queue_handler")
     if queue_handler is not None:
